eed8cfbeec5c15446fe9ade98e5ff157994590f7
ashbb <ashbbb@gmail.com>
2009-07-04 18:31:52 +0900

    uploaded a tool: insert codes into markdown files

---
+++ tools/insert_src.rb
@@ -0,0 +1,20 @@
+# insert_src.rb
+
+def read_src name
+  IO.readlines("../src/" + name).collect{|c| "\t" + c}
+end
+
+def insert_src path
+  Dir.glob(path).each do |file|
+    lines = IO.readlines file
+    open(file, 'w') do |f|
+      lines.each do |line|
+        new_line = line
+        line.sub(/^# *(.*\.rb)/){new_line = read_src $1}
+        f.puts new_line
+      end
+    end
+  end
+end
+
+insert_src "../md/*.md"
